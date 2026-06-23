d0bdbaea3fd9e8e2c82a5b544990db734dc07487
sivagiri s <sivagiriece29@gmail.com>
2026-06-23 19:22:40 +0530

    Time: 0 ms (100%), Space: 9 MB (67.6%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -0,0 +1,21 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    int l=0;
+    int h=numsSize-1;
+    while(l<=h)
+    {
+        int mid=l+(h-l)/2;
+        if(nums[mid]==target)
+        {
+            return mid;
+        }
+        else if(nums[mid]<target)
+        {
+            l=mid+1;
+        }
+        else
+        {
+            h=mid-1;
+        }
+    }
+    return l;
+}
